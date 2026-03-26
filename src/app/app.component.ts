0f53c6935b179b18ad0136545c53c35ceb6dcf7a
APSARA-AMRI <apsaraamri@gmail.com>
2026-03-26 12:06:32 +0530

    navbar component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'my-app';
+  title:string='hello';
+ count:number = 0;
+ userName = 'Apsara';
+ incrementCount(){
+  this.count++;
+ }
 }
